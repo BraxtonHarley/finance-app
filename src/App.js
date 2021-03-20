4c849f0569c38f45738a24f1c09f6f37692978aa
Braxton Harley <braxton.2012.harley@gmail.com>
2021-03-19 23:29:19 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from './Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          
+          <Route path="/commontary">
+            <Header />
+            <h1>Commontary One</h1>
+          </Route>
+
+          <Route path="/main">
+            <Header />
+            <h1>Main . HTML</h1>
+          </Route>
+
+          <Route path="/">
+           <Header />
+           <h1>Home!</h1>
+          </Route>
+
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
